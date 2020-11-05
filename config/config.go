a1648936289b2b69d20ac707e5f1cdf3bbd6c967
Narender <muddagoni.n@gopherslab.com>
2020-11-05 15:17:06 +0530

    appconfig modified

--- config/config.go
+++ config/config.go
@@ -2,8 +2,8 @@ package config
 
 type AppConfig struct {
 	GrpcServerPort int    `env:"GRPC_SERVER_PORT,default=50060"`
-	Dbusername     string `env:"DB_USER,default=parikshitg"`
-	Dbpassword     string `env:"DB_PASS,default=parikshitg"`
+	Dbusername     string `env:"DB_USER,default=default"`
+	Dbpassword     string `env:"DB_PASS,default=default"`
 	Dbdatabase     string `env:"DB_DATABASE,default=gorpc"`
 	Dbport         int    `env:"DB_PORT,default=5432"`
 }
